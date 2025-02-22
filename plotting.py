90ab05b7e907fa9c59ac007c2d655934ac38983d
Rafee Chowdhury <rafeechowdhury3@gmail.com>
2025-02-22 15:05:30 -0700

    changes

--- plotting.py
+++ plotting.py
@@ -1,24 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
-# location "C:\Users\Owner\Desktop\Gesture Recognition\ghacks2025\data1.csv"
-# file = open("C:\\Users\\Owner\\Desktop\\Gesture Recognition\\ghacks2025\\data1.csv", "r")
-# print(file.read())
-
-arr = np.loadtxt("C:\\Users\\Owner\\Desktop\\Gesture Recognition\\ghacks2025\\data1.csv",
-                 delimiter=",")
-print(arr)
-
-axis = plt.axes(projection='3d')
-
-
-#x = 
-#y =
-#z =
-
-#axis.scatter(x, y, z)
-#axis.set_title("3D plot of land")
-#axis.set_xlabel("x-coordinate")
-#axis.set_ylabel("y-coordinate")
-#axis.set_zlabel("z-coordinate")
+df = pd.read_csv("C:\\Users\\Owner\\Desktop\\Gesture Recognition\\ghacks2025\\data1.csv")
+data_array = df.to_numpy()
+print(data_array)
+print(data_array[5][5])
 
