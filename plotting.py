b2a5185739bca9e0aedc47f7cda5d13134ccb12a
Rafee Chowdhury <rafeechowdhury3@gmail.com>
2025-02-22 14:22:38 -0700

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -1,16 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+arr = np.loadtxt("C:\\Users\\Owner\\Desktop\\Gesture Recognition\\ghacks2025\\data1.csv",
+                 delimiter=",", comments='#', skiprows=1)
+print(arr)
+
 axis = plt.axes(projection='3d')
 
-x = 
-y =
-z =
 
-axis.scatter(x, y, z)
-axis.set_title("3D plot of land")
-axis.set_xlabel("x-coordinate")
-axis.set_ylabel("y-coordinate")
-axis.set_zlabel("z-coordinate")
+#x = 
+#y =
+#z =
 
-plt.show()
+#axis.scatter(x, y, z)
+#axis.set_title("3D plot of land")
+#axis.set_xlabel("x-coordinate")
+#axis.set_ylabel("y-coordinate")
+#axis.set_zlabel("z-coordinate")
+
