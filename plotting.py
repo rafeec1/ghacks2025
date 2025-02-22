7926a7712fade3cfad44fb0e678767142c5bc1a5
Nafis245 <nafis5766@gmail.com>
2025-02-22 14:11:32 -0700

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -1,2 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+axis = plt.axes(projection='3d')
+
+x = 
+y =
+z =
+
+axis.scatter(x, y, z)
+axis.set_title("3D plot of land")
+axis.set_xlabel("x-coordinate")
+axis.set_ylabel("y-coordinate")
+axis.set_zlabel("z-coordinate")
+
+plt.show()
