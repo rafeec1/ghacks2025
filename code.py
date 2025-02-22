1b0e0b40e5157a7882ad8ce8e9b9a63bcafb26df
Rafee Chowdhury <rafeechowdhury3@gmail.com>
2025-02-22 13:52:10 -0700

    modified into csv file, and trying to put csv import

--- code.py
+++ code.py
@@ -1,1 +1,6 @@
-figure
+import csv
+
+with open('C:\Users\Owner\Desktop\Gesture Recognition\ghacks2025\Console_2025-02-22_12-31-51_data1.csv','r') as file:
+    coordinates_array = csv.reader(file)
+
+print(coordinates_array[5][4])
