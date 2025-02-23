6ccc7d4b2990e9034650dc5f8b065f4dda7e779b
Rafee Chowdhury <rafeechowdhury3@gmail.com>
2025-02-22 23:42:25 -0700

    added docstring and comments

--- plotting.py
+++ plotting.py
@@ -3,86 +3,119 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import pandas as pd
 
-
-
-
-
 def average(data1):
-    latitude_average_num = 0
+    """ Function takes in a numpy array of all collected data, and returns the average of every 10 data points to three dedicated lists
+        In this case, returns the average value of latitude, longitude, and height from receiver data.
+        Parameters:
+            data1 (numpy array): 
+                an array containing data collected from the Hexagon receiver using "log psrposa ontime 0.2"
+
+        Returns:
+            average_latitude (list): 
+                list containing averaged latitude data.
+
+            average_longitude (list): 
+                list containing averaged longitude data.
+
+            average_height (list): 
+                list containing averaged height data.
+
+    """
+    latitude_average_num = 0        # initializing average sums to zero
     longitude_average_num = 0
     height_average_num = 0
 
-    average_latitude = []
+    average_latitude = []           # lists for averaged data
     average_longitude = []
     average_height = []
 
-    for i in range(0, len(data1)-10, 10):
-        for j in range(10):
-            latitude_average_num += data1[i+j][11]
-            longitude_average_num += data1[i+j][12]
+    for i in range(0, len(data1)-10, 10):               # looping through every 10 lines from data1               
+        for j in range(10):                             
+            latitude_average_num += data1[i+j][11]      # adds every consecutive latitude point up til 10
+            longitude_average_num += data1[i+j][12]     # same for longitude and height
             height_average_num += data1[i+j][13]
          
-        average_latitude.append(latitude_average_num / 10)
-        average_longitude.append(longitude_average_num / 10)
+        average_latitude.append(latitude_average_num / 10)      # calculating mean latitude and adds it to the average lat list
+        average_longitude.append(longitude_average_num / 10)    # same for long and height
         average_height.append(height_average_num / 10)
 
-        # resets values for average to 0
-        latitude_average_num = 0
+        latitude_average_num = 0                        # resets sum to find the next mean values 
         longitude_average_num = 0
         height_average_num = 0
     return average_latitude, average_longitude, average_height
 
-df = pd.read_csv("C:\\Users\\Owner\\Desktop\\Gesture Recognition\\ghacks2025\\LatLongHeightdata.csv")
-data_array = df.to_numpy()
+def remove_outliers(lat, longg, height):
+    """ Takes in averaged data and removes any heights that are obvious outliers (less than 1105 or greater than 1125 m)
+        Parameters:
+            lat (list): list containing average latitudes
+            longg (list): list containing average longitudes
+            height (list): list containing average heights
+        
+        returns:
+            lat (list): 
+            longg (list):
+            height_copy (list):
+            same lists but outlier data removed
+    """
 
-
-lat, longg, height = average(data_array)
-
-adjusted=0
-height_copy = height[:]
-for i in range(len(height)):
-    if (height[i] <=1105 or height[i]>= 1125):
-        height_copy.pop(i-adjusted)
-        longg.pop(i-adjusted)
-        lat.pop(i-adjusted)
-        adjusted+=1
-
-
-def plotData():
+    adjusted=0                                          # counter
+    height_copy = height[:]                             # makes copy of height to remove values from
+    for i in range(len(height)):                        # iterates over original height list
+        if (height[i] <=1105 or height[i]>= 1125):      # outliers defined here
+            height_copy.pop(i-adjusted)                 # pops the outlying point from each data list
+            longg.pop(i-adjusted)
+            lat.pop(i-adjusted)
+            adjusted+=1                                 # counter goes up to account for index change
     
-axis = plt.axes(projection='3d')
+    return lat, longg, height_copy
+
+def plot_data(lat, longg, height):
+    """ plots and shows a 3D scatter plot of given spacial data using matplotlib
+        Parameters:
+            lat (list): list of latitude data
+            longg (list): list of longitude data
+            height (list): list of height data
+        Returns:
+            None 
+    """
     
-axis.scatter(lat, longg, height_copy, alpha = 0.5)
 
-# constants for plotting latitude
-min_lat = min(lat)
-max_lat = max(lat)
-lat_interval = (max_lat - min_lat) / 5
-lat_labels = np.arange(min_lat, max_lat, lat_interval)
+    min_lat = min(lat)                      # constants for plotting latitude
+    max_lat = max(lat)
+    lat_interval = (max_lat - min_lat) / 5
+    lat_labels = np.arange(min_lat, max_lat, lat_interval)
 
-# constants for plotting longitude
-min_longg = min(longg)
-max_longg = max(longg)
-longg_interval = (max_longg - min_longg) / 5
-longg_labels = np.arange(min_longg, max_longg, longg_interval)
+    min_longg = min(longg)                  # constants for plotting longitude
+    max_longg = max(longg)
+    longg_interval = (max_longg - min_longg) / 5
+    longg_labels = np.arange(min_longg, max_longg, longg_interval)
 
-# building the plot
-axis.set_title("3D plot of land")
+    # building the plot
+    axis = plt.axes(projection='3d')    
+    axis.scatter(lat, longg, height, alpha = 0.5)
 
-axis.set_xlabel("latitude", labelpad=20)
-axis.set_xticks(lat_labels)
+    axis.set_title("3D plot of land")
+    axis.set_xlabel("latitude", labelpad=20)
+    axis.set_xticks(lat_labels)
+    axis.set_ylabel("longitude", labelpad=20)
+    axis.set_yticks(longg_labels)
+    axis.set_zlabel("altitude (m)", labelpad=20)
+    axis.set_zlim(1000, 1150)
 
-axis.set_ylabel("longitude", labelpad=20)
-axis.set_yticks(longg_labels)
+    plt.show()
 
-axis.set_zlabel("altitude (m)", labelpad=20)
-axis.set_zlim(1000, 1150)
+def main(positional_data):
+    """ program that plots positional data from a Hexagon GNSS receiver in a graphic.
+        Parameters:
+            positional_data (string): a csv file with data from a GNSS receiver
+        Returns:
+            a matplotlib plot
+    """
+    df = pd.read_csv(positional_data)
+    data_array = df.to_numpy()
+    lat, longg, height = average(data_array)
+    cleaned_lat, cleaned_longg, cleaned_height = remove_outliers(lat, longg, height)
+    plot_data(cleaned_lat, cleaned_longg, cleaned_height)
 
-
-
-
-
-
-
-
-plt.show()
+if __name__ == '__main__':
+    main("C:\\Users\\Owner\\Desktop\\Gesture Recognition\\ghacks2025\\LatLongHeightdata.csv")
